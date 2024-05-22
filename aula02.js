7b7e264ab99b3e1f0635dea8631deec2dc2561cf
gustavoroberto1 <gustavo.roberto@edu.sc.senai.br>
2024-05-22 19:44:29 -0300

    teste

--- aula02.js
+++ aula02.js
@@ -5,7 +5,5 @@ var dataNascimento
 
 document.write(typeof nome, '<br/>')
 document.write(typeof dataNascimento, '<br/>')
-document.write(typeof idade, '<br/>')
-document.write(typeof eProfessor, '<br/>')
 
 
