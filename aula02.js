0b270daa5b05344999752b4b78dcfc8bd526bca7
gustavoroberto1 <gustavo.roberto@edu.sc.senai.br>
2024-05-22 19:47:29 -0300

    apagado arquivo

--- aula02.js
+++
@@ -1,9 +0,0 @@
-var nome = 'Gustavo Roberto de Souza'
-let idade = 26.5
-const eProfessor = true
-var dataNascimento
-
-document.write(typeof nome, '<br/>')
-document.write(typeof dataNascimento, '<br/>')
-
-
